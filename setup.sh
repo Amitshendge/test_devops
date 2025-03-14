25aaafd15b8e7cdb88511a6269ddcc4e3f1b9a01
Amit Shendge <92355828+Amitshendge@users.noreply.github.com>
2025-03-14 15:10:37 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,19 +7,19 @@ sudo apt update
 sudo apt install -y python3
 
 # Create a virtual environment
-python3 -m venv venv
+#python3 -m venv venv
 
 # Activate the virtual environment
-source venv/bin/activate
+#source venv/bin/activate
 
 # Upgrade pip to the latest version
-pip install --upgrade pip
+#pip install --upgrade pip
 
 # Install dependencies from requirements.txt (if exists)
-if [ -f requirements.txt ]; then
-    pip install -r requirements.txt
-else
-    echo "requirements.txt not found, skipping dependency installation."
-fi
+#if [ -f requirements.txt ]; then
+#    pip install -r requirements.txt
+#else
+#    echo "requirements.txt not found, skipping dependency installation."
+#fi
 
-echo "Python setup complete. Virtual environment is ready."
+#echo "Python setup complete. Virtual environment is ready."

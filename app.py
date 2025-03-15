90cd321125ae30ed574b6b418fab8c19d5945bff
Amit Shendge <92355828+Amitshendge@users.noreply.github.com>
2025-03-14 17:15:05 -0700

    updated folder structure

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Set the title of the app
-st.title("Myy First Streamlit App")
+st.title("My My First Streamlit App")
 # Add a header
 st.header("Welcome to my app!")
 # Add a subheader
